0dd8c08ab4a4652f67dd25ec3e766b9586b2942e
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-07-04 16:45:27 -0300

    Novo exercicio

---
+++ aula12ex/script/modelo.js
@@ -0,0 +1,24 @@
+alert('Olá, seja bem vindo!')
+
+function carregar(){}
+var msg = document.getElementById('msg')
+var img = document.getElementById ('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+
+if (hora >=0 && hora <= 12){
+    // Bom dia!
+    img.src = 'imagens/fotomanha.png'
+    document.body.style.background = '#AFBAB7'
+    
+} else if (hora >= 12 && hora < 18){
+    // Boa tarde!
+    img.src = 'imagens/fototarde.png'
+    document.body.style.background = '#D08954'
+} else{
+    img.src = 'imagens/fotonoite.png'
+    document.body.style.background = '#856FAB'
+    // Boa noite!
+}
+
