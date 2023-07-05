74740fe292606183e0a1a3a3af018e09b1f7be22
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-07-05 16:45:09 -0300

    update

---
+++ idade/src/JS/modelo.js
@@ -0,0 +1,11 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = Number(document.getElementById('txtano').value)
+    var res = document.getElementById('res')
+    if (fano.length == 0 || (fano) > ano){
+        window.alert(`[ERRO] Verifique os dados e tente novamente!`)
+    }else{
+        var fsex = 
+    }
+}
--- nova-atividade01/src/JS/modelo.js
+++ nova-atividade01/src/JS/modelo.js
@@ -1,11 +1,11 @@
-alert('Olá, seja bem vindo!')
-
 function carregar(){}
 var msg = document.getElementById('msg')
 var img = document.getElementById ('imagem')
 var data = new Date()
 var hora = data.getHours()
+
 msg.innerHTML = `Agora são ${hora} horas.`
+
 
 if (hora >=0 && hora <= 12){
     // Bom dia!
