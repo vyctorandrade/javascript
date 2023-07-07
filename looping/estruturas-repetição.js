1686aa309072339b5770f5eac7e349356ddea813
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-07-07 13:08:04 -0300

    Update estruturas-repetição.js

--- looping/estruturas-repetição.js
+++ looping/estruturas-repetição.js
@@ -34,6 +34,6 @@ do{
 
 
 
-// Modo de depuração - Ele executa passo a passo as linhas de código mostrando Onde entrar: Em opções executar e depois iniciar depuração!
+// Modo de depuração - Ele executa passo a passo as linhas de código mostrando se tem erro de lógica ou não. ///// Mostrando Onde entrar: Em opções executar e depois iniciar depuração!
 
 // Para usar: Primeiro marca com bola vermelha a linha a ser executada de início, abre o console e depois o console de depuração. Em seguida aperta F10 ou menu que aparece acima na opção contornar ou F10. Nisso vai executando cada linha e vendo o passo a passo.
