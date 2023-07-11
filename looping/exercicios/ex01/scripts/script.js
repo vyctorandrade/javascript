3f0e193f6e5d3e62d3c31be0c488e508b69b607c
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-07-11 08:31:38 -0300

    Finish

--- looping/exercicios/ex01/scripts/script.js
+++ looping/exercicios/ex01/scripts/script.js
@@ -3,20 +3,16 @@ window.confirm(`Olá, seja bem vindo! Instruções para o super contador:
 2° Agora o número final, ele será o fim da contagem. 
 3° Por fim de quantas em quantas casas irá pular.`)
 function contar(){
-    var inicio = Number(document.getElementById('inicio').value)
-    var fim = Number(document.getElementById ('fim').value)
-    var passo = Number (document.getElementById ('passo').value)
+    var i = Number(document.getElementById('inicio').value)
+    var f = Number(document.getElementById ('fim').value)
+    var p = Number (document.getElementById ('passo').value)
     var res = document.getElementById ('res')
 
-    if (inicio == 0 || fim == 0 || passo == 0 ){
+    if (i == 0 || f == 0 || p == 0 ){
         // Pode por em mensagem na tela junto // res.innerHTML = `Impossível contar!`
         window.alert('[Erro] Faltam dados!')
     } else {
         res.innerHTML = 'Contando:'
-        var i = inicio
-        var f = fim
-        var p = passo
-
         if (i < f){
             // contagem crescente
             for(var c = i; c <= f; c+=p){
@@ -24,7 +20,7 @@ function contar(){
             }
            
     } else {
-        // Contagem refressiva
+        // Contagem regressiva
         for (var c = i; c >= f; c-= p){
             res.innerHTML += `${c} \u{1F449} `
          }
@@ -32,3 +28,7 @@ function contar(){
     }
     res.innerHTML += `\u{1F3C1}`
     }}
+
+
+    // Depois tentar entender a lógica do  +=
+    // += não substitui somente acrescenta.
